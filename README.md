ad3ae8aef06e60b325fb60c27497fa55e4c65388
amsang <sang-uk.lee@stonybrook.edu>
2020-11-08 02:18:51 +0900

    Fix main page content

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 2. `$ yarn`
 3. Install ESLiint and Prettier VSCode Plugin
    - See [this link](https://velog.io/@velopert/eslint-and-prettier-in-react) for better coding experience
+4. To build and deploy to github pages site, `$ yarn deploy`
--- src/components/Main.js
+++ src/components/Main.js
@@ -6,8 +6,18 @@ const Main = () => {
       <h3>CAGO Playground</h3>
       <p>for Junction 2020 Connected</p>
       <pre>
-        <a href="http://sang-uk.kr/">Sean (Sang-Uk) Lee</a>
-        {` | sean.sanguk.lee@gmail.com\nJongwon Park\nDaekyoung Kim\nand\nHanbin Baik`}
+        <a href="https://www.linkedin.com/in/sang-uk-lee/">
+          Sean (Sang-Uk) Lee
+        </a>
+        {` | sean.sanguk.lee@gmail.com\n`}
+        <a href="https://www.linkedin.com/in/jongwon-park-41a170177">
+          Jongwon Park
+        </a>
+        {` | whddnjs761@gmail.com\n`}
+        <a href="https://www.linkedin.com/in/timkim0713/">Daekyung Kim</a>
+        {` | daekyung.kim@stonybrook.edu\n`}
+        <a href="https://linkedin.com/in/hanbin-baik">Hanbin Baik</a>
+        {` | hanbini96@gmail.com`}
       </pre>
       <p>Nov. 6th ~ Nov. 8th, 2020</p>
     </div>
