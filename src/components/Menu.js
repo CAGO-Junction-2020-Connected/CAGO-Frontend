a6a87a78bcf0fb581cb89df6ee3c8956cc6bd325
HanBin Baik <hanbini96@gmail.com>
2020-11-07 14:41:08 -0500

    Login added

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const Menu = () => {
+  function getUser() {
+    console.log("Getting user")
+    axios.get('http://localhost:3001/db/products', {
+      withCredentials: true})
+      .then(res => {
+        console.log(res.data)
+      })
+  }
+
   return (
     <>
       <ul>
@@ -11,6 +21,11 @@ const Menu = () => {
         <li>
           <Link to="/chart">Chart</Link>
         </li>
+        <li>
+          <Link to="/chart">Chart</Link>
+        </li>
+        <a href="http://localhost:3001/auth/login">Login</a>
+        <button type="button" onClick={getUser}>Get User</button>
       </ul>
       <hr />
     </>
